07e2a9c6bb427be89e11c324d436084d4f64cb82
Tomáš Konrády <tomaskonrady@gmail.com>
2018-10-30 11:56:49 +0100

    ignore build folders

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
 	bail: true,
 	verbose: true,
 	testPathIgnorePatterns: ['/.history/', '/node_modules/', '/es', '/dist', '/lib', '/rollup'],
-	setupTestFrameworkScriptFile: '<rootDir>/testsSetup.js',
 	projects: ['<rootDir>/packages/*'],
-	snapshotSerializers: ['enzyme-to-json/serializer'],
 };

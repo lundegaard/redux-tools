5f212d55851e3601f560b08accd10e82a8b445a2
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-21 14:19:53 +0100

    Fix enzyme in tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
 	bail: true,
 	verbose: true,
 	testPathIgnorePatterns: ['/.history/', '/node_modules/', '/es', '/dist', '/lib', '/rollup'],
-	projects: ['<rootDir>/packages/*'],
+	snapshotSerializers: ['enzyme-to-json/serializer'],
+	setupTestFrameworkScriptFile: '<rootDir>/tests/enzymeSetup.js',
 };

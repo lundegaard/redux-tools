74c2f0e71521ed70cd5986129e66d40124fe92b8
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-23 15:31:27 +0100

    Refactor reducers package

--- packages/reducers/src/enhancer.js
+++ packages/reducers/src/enhancer.js
@@ -1,45 +1,37 @@
-import { forEachObjIndexed, assocPath, keys, dissocPath, forEach, map } from 'ramda';
+import { both, keys, concat, reject } from 'ramda';
+import { createEntries, isVersionEjectable, isEntryIncluded } from '@redux-tools/injectors';
 
 import { reducersInjected, reducersEjected } from './actions';
-import { makeRootReducer, getReducerPath, filterReducer } from './reducers';
+import combineReducerEntries from './combineReducerEntries';
 
 export default function enhancer() {
 	return createStore => (...args) => {
 		const store = createStore(...args);
 
-		store.injectedReducers = {
-			namespaces: {},
+		store.reducerEntries = [];
+
+		store.injectReducers = (reducers, namespace, version) => {
+			store.reducerEntries = concat(
+				store.reducerEntries,
+				createEntries(reducers, namespace, version)
+			);
+
+			store.replaceReducer(combineReducerEntries(store.reducerEntries));
+			store.dispatch(reducersInjected({ reducers: keys(reducers), namespace, version }));
 		};
 
-		const injectReducers = (reducers, namespace) => {
-			const injectReducer = (reducer, key) =>
-				(store.injectedReducers = assocPath(
-					getReducerPath(key, namespace),
-					reducer,
-					store.injectedReducers
-				));
+		store.ejectReducers = (reducers, namespace, version) => {
+			store.reducerEntries = reject(
+				both(
+					isVersionEjectable(version),
+					isEntryIncluded(createEntries(reducers, namespace, version))
+				),
+				store.reducerEntries
+			);
 
-			const filteredReducers = map(filterReducer(namespace), reducers);
-			forEachObjIndexed(injectReducer, filteredReducers);
-			store.replaceReducer(makeRootReducer(store.injectedReducers));
-			store.dispatch(reducersInjected({ reducers: keys(filteredReducers), namespace }));
+			store.replaceReducer(combineReducerEntries(store.reducerEntries));
+			store.dispatch(reducersEjected({ reducers: keys(reducers), namespace, version }));
 		};
-
-		const ejectReducers = (reducers, namespace) => {
-			const ejectReducer = key =>
-				(store.injectedReducers = dissocPath(
-					getReducerPath(key, namespace),
-					store.injectedReducers
-				));
-
-			const reducerKeys = keys(reducers);
-			forEach(ejectReducer, reducerKeys);
-			store.replaceReducer(makeRootReducer(store.injectedReducers));
-			store.dispatch(reducersEjected({ reducers: reducerKeys, namespace }));
-		};
-
-		store.injectReducers = injectReducers;
-		store.ejectReducers = ejectReducers;
 
 		return store;
 	};
--- packages/reducers/src/enhancer.test.js
+++ packages/reducers/src/enhancer.test.js
@@ -1,8 +1,6 @@
-import { o, defaultTo, identity } from 'ramda';
 import enhancer from './enhancer';
 
 const createStore = jest.fn(() => ({}));
-const reducer = o(defaultTo(null), identity);
 
 describe('enhancer', () => {
 	beforeEach(() => jest.clearAllMocks());
@@ -13,41 +11,5 @@ describe('enhancer', () => {
 		expect(store.ejectReducers).toBeInstanceOf(Function);
 	});
 
-	it('populates injectedReducers upon injecting a new one and calls appropriate store methods', () => {
-		const store = enhancer()(createStore)();
-		store.replaceReducer = jest.fn();
-		store.dispatch = jest.fn();
-		store.injectReducers({ foo: reducer });
-		expect(store.injectedReducers.foo).toBeInstanceOf(Function);
-		expect(store.replaceReducer).toHaveBeenCalled();
-		expect(store.dispatch).toHaveBeenCalled();
-	});
-
-	it('populates injectedReducers upon injecting a new one (namespaced)', () => {
-		const store = enhancer()(createStore)();
-		store.replaceReducer = jest.fn();
-		store.dispatch = jest.fn();
-		store.injectReducers({ foo: reducer }, 'ns');
-		expect(store.injectedReducers.namespaces.ns.foo).toBeInstanceOf(Function);
-	});
-
-	it('filters injectedReducers upon ejection and calls appropriate store methods', () => {
-		const store = enhancer()(createStore)();
-		store.replaceReducer = jest.fn();
-		store.dispatch = jest.fn();
-		store.injectedReducers = { foo: reducer };
-		store.ejectReducers({ foo: identity });
-		expect(store.injectedReducers.foo).toBeUndefined();
-		expect(store.replaceReducer).toHaveBeenCalled();
-		expect(store.dispatch).toHaveBeenCalled();
-	});
-
-	it('filters injectedReducers upon ejection (namespaced)', () => {
-		const store = enhancer()(createStore)();
-		store.replaceReducer = jest.fn();
-		store.dispatch = jest.fn();
-		store.injectedReducers = { namespaces: { ns: { foo: reducer } } };
-		store.ejectReducers({ foo: identity }, 'ns');
-		expect(store.injectedReducers.namespaces.ns).toEqual({});
-	});
+	// TODO: Enhancer tests
 });

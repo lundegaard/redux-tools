55c4a744a219ff8fd9b442be152318ac5d623422
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-23 20:05:03 +0100

    Fix isEntryIncluded and isEntryIncludedTimes tests

--- packages/injectors/src/isEntryIncluded.test.js
+++ packages/injectors/src/isEntryIncluded.test.js
@@ -9,12 +9,12 @@ describe('isEntryIncluded', () => {
 	it('returns true for an entry which is included', () => {
 		expect(
 			isEntryIncluded(entries, { key: 'foo', injectable: 'bar', namespace: 'ns', version: 1 })
-		);
+		).toBe(true);
 	});
 
 	it('returns false for an entry which is not included', () => {
 		expect(
 			isEntryIncluded(entries, { key: 'LOL', injectable: 'NOPE', namespace: 'ns', version: 1 })
-		);
+		).toBe(false);
 	});
 });

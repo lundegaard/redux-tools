784618465a3770c203ebe6f0f25df7bee7a4ea4c
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-23 21:28:34 +0100

    Fix isVersionEjectable

--- packages/injectors/src/isVersionEjectable.js
+++ packages/injectors/src/isVersionEjectable.js
@@ -1,5 +1,4 @@
-import { curry } from 'ramda';
-import { isNotNil } from 'ramda-extension';
+import { curry, isNil } from 'ramda';
 
 /**
  * Returns whether the tested version is not higher than the reference version.
@@ -8,9 +7,16 @@ import { isNotNil } from 'ramda-extension';
  * @param {?number} testedVersion version to test
  * @returns {boolean} whether the version can be ejected
  */
-const isVersionEjectable = curry(
-	(referenceVersion, testedVersion) =>
-		isNotNil(referenceVersion) && isNotNil(testedVersion) && testedVersion <= referenceVersion
-);
+const isVersionEjectable = curry((referenceVersion, testedVersion) => {
+	if (isNil(referenceVersion) && isNil(testedVersion)) {
+		return true;
+	}
+
+	if (isNil(referenceVersion) || isNil(testedVersion)) {
+		return false;
+	}
+
+	return testedVersion <= referenceVersion;
+});
 
 export default isVersionEjectable;
--- packages/injectors/src/isVersionEjectable.test.js
+++ packages/injectors/src/isVersionEjectable.test.js
@@ -1,11 +1,13 @@
 import isVersionEjectable from './isVersionEjectable';
 
 describe('isVersionEjectable', () => {
-	it('returns false if one of the arguments is undefined', () => {
+	it('returns true if both arguments are nil', () => {
+		expect(isVersionEjectable(null, null)).toBe(true);
+	});
+
+	it('returns false if exactly one argument is nil', () => {
 		expect(isVersionEjectable(null, 0)).toBe(false);
-		expect(isVersionEjectable(null, 1)).toBe(false);
 		expect(isVersionEjectable(0, null)).toBe(false);
-		expect(isVersionEjectable(1, null)).toBe(false);
 	});
 
 	it('returns true if the version can be ejected', () => {

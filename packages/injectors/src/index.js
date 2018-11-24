88a65cb0f298775982593d2859da62f208789ee1
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-24 09:44:32 +0100

    Fix ejecting by version

--- packages/injectors/src/index.js
+++ packages/injectors/src/index.js
@@ -1,5 +1,5 @@
 export { default as areEntriesEqual } from './areEntriesEqual';
 export { default as createEntries } from './createEntries';
+export { default as isEntryEjectableByVersion } from './isEntryEjectableByVersion';
 export { default as isEntryIncluded } from './isEntryIncluded';
 export { default as isEntryIncludedTimes } from './isEntryIncludedTimes';
-export { default as isVersionEjectable } from './isVersionEjectable';

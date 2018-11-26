e853540e68ad9fbe85f7def832fac8ae9762af66
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-26 14:50:20 +0100

    Stream creators and utils docs

--- packages/stream-creators/src/streamCreators.js
+++ packages/stream-creators/src/streamCreators.js
@@ -2,7 +2,15 @@ import { map } from 'rxjs/operators';
 import { prop } from 'ramda';
 import { getStateByNamespace } from '@redux-tools/reducers';
 
+/**
+ * Stream creator to pass as `streamCreator` to the enhancer. Adds a `namespacedState$` argument
+ * to each epic, allowing access to state based on the namespace of the epic.
+ */
 export const namespacedState$ = ({ namespace, state$ }) =>
 	state$.pipe(map(getStateByNamespace(namespace)));
 
+/**
+ * Stream creator to pass as `streamCreator` to the enhancer. Adds a `globalAction$` argument
+ * to each epic, allowing access to actions of all namespaces, not just the epic's.
+ */
 export const globalAction$ = prop('globalAction$');

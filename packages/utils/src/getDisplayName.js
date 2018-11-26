466a016d9fd62013c3c66bb8f62bc21daddc627b
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-26 11:17:59 +0100

    Add docs to utils

--- packages/utils/src/getDisplayName.js
+++ packages/utils/src/getDisplayName.js
@@ -1,1 +1,14 @@
-export default Component => Component.displayName || Component.name || 'Component';
+import { prop, unless, both, always } from 'ramda';
+import { isString, isNotEmpty, dispatch } from 'ramda-extension';
+
+/**
+ * Returns the display name of a React component, falling back appropriately.
+ *
+ * @param {React.Component} Component React component to get the name of
+ * @returns {string}
+ */
+export default dispatch([
+	prop('displayName'),
+	prop('name'),
+	unless(both(isString, isNotEmpty), always('Component')),
+]);
--- packages/utils/src/getDisplayName.test.js
+++ packages/utils/src/getDisplayName.test.js
@@ -4,6 +4,7 @@ describe('getDisplayName', () => {
 	it('returns the most appropriate display name', () => {
 		expect(getDisplayName({ displayName: 'yo', name: 'hi' })).toBe('yo');
 		expect(getDisplayName({ name: 'hi' })).toBe('hi');
+		expect(getDisplayName('div')).toBe('div');
 		expect(getDisplayName({})).toBe('Component');
 	});
 });

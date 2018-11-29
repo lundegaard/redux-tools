0af390a2a1cd1776dfa6806f8e836d6a097a1749
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-29 15:05:57 +0100

    Namespaces docs

--- packages/namespaces/src/attachNamespace.js
+++ packages/namespaces/src/attachNamespace.js
@@ -1,5 +1,11 @@
 import { curry, assocPath } from 'ramda';
 
+/**
+ * Associates an action with a namespace.
+ *
+ * @param {?string} namespace namespace to attach
+ * @param {Object} action action to add the namespace to
+ */
 const attachNamespace = curry(
 	(namespace, action) => (namespace ? assocPath(['meta', 'namespace'], namespace, action) : action)
 );

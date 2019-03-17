ddc5875184afc1ed03d67c887268a60d08406de6
Václav Jančařík <vaclav.janc@gmail.com>
2019-03-17 13:15:43 +0100

    Fix stream creators after adding features

--- packages/stream-creators/src/streamCreators.js
+++ packages/stream-creators/src/streamCreators.js
@@ -6,8 +6,8 @@ import { getStateByNamespace } from '@redux-tools/reducers';
  * Stream creator to pass as `streamCreator` to the enhancer. Adds a `namespacedState$` argument
  * to each epic, allowing access to state based on the namespace of the epic.
  */
-export const namespacedState$ = ({ namespace, state$ }) =>
-	state$.pipe(map(getStateByNamespace(namespace)));
+export const namespacedState$ = ({ feature, namespace, state$ }) =>
+	state$.pipe(map(getStateByNamespace(feature, namespace)));
 
 /**
  * Stream creator to pass as `streamCreator` to the enhancer. Adds a `globalAction$` argument

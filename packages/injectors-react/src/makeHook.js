618ac02ce9fb9e9f946399c0586b68d4231ce771
Václav Jančařík <vaclav.janc@gmail.com>
2019-04-13 21:23:44 +0200

    Rename injector options

--- packages/injectors-react/src/makeHook.js
+++ packages/injectors-react/src/makeHook.js
@@ -1,6 +1,6 @@
-import { useEffect, useState, useMemo } from 'react';
-import { all, keys, includes } from 'ramda';
-import { toPascalCase, alwaysEmptyArray } from 'ramda-extension';
+import { useEffect, useState } from 'react';
+import { all, keys, includes, isNil } from 'ramda';
+import { toPascalCase, alwaysEmptyArray, isNotNil } from 'ramda-extension';
 import invariant from 'invariant';
 import { DEFAULT_FEATURE } from '@redux-tools/namespaces';
 import { createEntries } from '@redux-tools/injectors';
@@ -13,32 +13,42 @@ const makeHook = (configuration = {}) => (injectables, options = {}) => {
 	invariant(eject, 'The ejection handler must be defined.');
 	invariant(inject, 'The injection handler must be defined.');
 
-	const { global = false, feature = DEFAULT_FEATURE, persist = false } = options;
+	const { feature = DEFAULT_FEATURE } = options;
+	const isGlobal = options.isGlobal || options.global || false;
+	const shouldEject = isNil(options.shouldEject) || options.shouldEject || !options.persist;
+
 	const { store, namespace } = useInjectorContext(feature);
 
-	const areEntriesAlreadyIncluded = useMemo(
-		() => all(entry => includes(entry, getEntries(store)), createEntries(injectables)),
-		[store, getEntries, injectables]
+	const [isInitialized, setIsInitialized] = useState(() =>
+		all(entry => includes(entry, getEntries(store)), createEntries(injectables, { feature }))
 	);
 
-	const [isInitialized, setIsInitialized] = useState(areEntriesAlreadyIncluded);
-
 	useEffect(() => {
-		const resolvedNamespace = global ? null : namespace;
+		const resolvedNamespace = isGlobal ? null : namespace;
 		const hookName = `use${toPascalCase(type)}`;
 		const locationMessage = `Attempting to inject following ${type}: ${keys(injectables)}.`;
 
 		invariant(
-			!feature || !global,
-			`${locationMessage} You cannot pass a feature to global injectables.`
+			!feature || !isGlobal,
+			`${locationMessage} You cannot pass a feature to global ${type}.`
 		);
 
-		if (!resolvedNamespace && !global) {
+		if (!resolvedNamespace && !isGlobal) {
 			console.warn(
 				locationMessage,
-				"You're using a @redux-tools injector with 'global: false' and no namespace! " +
-					'It will behave like a global injector. If this is intended, consider passing ' +
-					`'global: true' to the injector, e.g. '${hookName}(${type}, { global: true })'.`
+				"You're using a @redux-tools injector with no namespace!",
+				'It will behave like a global injector. If this is intended, consider passing',
+				`'isGlobal: true' to the injector, e.g. '${hookName}(${type}, { isGlobal: true })'.`
+			);
+		}
+
+		if (isNotNil(options.global)) {
+			console.warn(`'global: ${options.global}' is deprecated. Use 'isGlobal: ${options.global}'.`);
+		}
+
+		if (isNotNil(options.persist)) {
+			console.warn(
+				`'persist: ${options.persist}' is deprecated. Use 'shouldEject: ${!options.persist}'.`
 			);
 		}
 
@@ -50,7 +60,7 @@ const makeHook = (configuration = {}) => (injectables, options = {}) => {
 		setIsInitialized(true);
 
 		return () => {
-			if (!persist) {
+			if (shouldEject) {
 				eject(store)(injectables, {
 					namespace: resolvedNamespace,
 					feature,

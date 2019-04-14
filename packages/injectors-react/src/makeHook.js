14fa2cf879c416b3185758375df6cc5f05ee17eb
Václav Jančařík <vaclav.janc@gmail.com>
2019-04-14 15:30:58 +0200

    Add useDebugValue to makeHook

--- packages/injectors-react/src/makeHook.js
+++ packages/injectors-react/src/makeHook.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState, useEffect } from 'react';
+import { useLayoutEffect, useState, useEffect, useDebugValue } from 'react';
 import { keys, all, includes, omit } from 'ramda';
 import { toPascalCase, isNotNil, rejectNil } from 'ramda-extension';
 import invariant from 'invariant';
@@ -21,89 +21,97 @@ const makeHook = type => {
 
 	const hookName = `use${toPascalCase(type)}`;
 
-	// HACK: https://stackoverflow.com/questions/5905492/dynamic-function-name-in-javascript
-	const useInjectables = {
-		[hookName]: (injectables, options = {}) => {
-			const locationMessage =
-				`@redux-tools: This warning happened while injecting the following ${type}: ` +
-				`${keys(injectables)}.`;
+	const useInjectables = (injectables, options = {}) => {
+		const injectableKeys = keys(injectables);
+		const locationMessage =
+			`@redux-tools: This warning happened while injecting the following ${type}: ` +
+			`${injectableKeys}.`;
 
-			const warn = (...args) => console.warn(locationMessage, ...args);
+		const warn = (...args) => console.warn(locationMessage, ...args);
 
-			// NOTE: `options.global` and `options.persist` are deprecated.
-			const isGlobal = options.isGlobal || options.global || false;
-			const isPersistent = options.isPersistent || options.persist || false;
-			const feature = options.feature || DEFAULT_FEATURE;
-			const injectorContext = useInjectorContext(feature);
-			const { store } = injectorContext;
-			const namespace = isGlobal ? null : options.namespace || injectorContext.namespace;
+		// NOTE: `options.global` and `options.persist` are deprecated.
+		const isGlobal = options.isGlobal || options.global || false;
+		const isPersistent = options.isPersistent || options.persist || false;
+		const feature = options.feature || DEFAULT_FEATURE;
+		const injectorContext = useInjectorContext(feature);
+		const { store } = injectorContext;
+		const namespace = isGlobal ? null : options.namespace || injectorContext.namespace;
 
-			// NOTE: On the server, the injectables should be injected beforehand.
-			const [isInitialized, setIsInitialized] = useState(IS_SERVER);
+		// NOTE: On the server, the injectables should be injected beforehand.
+		const [isInitialized, setIsInitialized] = useState(IS_SERVER);
 
-			const props = rejectNil({
-				...getOtherProps(options),
-				feature,
-				namespace,
-			});
+		const props = rejectNil({
+			...getOtherProps(options),
+			feature,
+			namespace,
+		});
 
-			if (IS_SERVER) {
-				const areEntriesAlreadyInjected = all(
-					entry => includes(entry, getEntries(store)),
-					createEntries(injectables, props)
+		// TODO: Refactor when React DevTools support multiple debug values or non-primitive structures.
+		useDebugValue(
+			String([
+				`Namespace: ${namespace}`,
+				`Feature: ${feature}`,
+				`${toPascalCase(type)}: ${injectableKeys}`,
+				`Initialized: ${isInitialized}`,
+			])
+		);
+
+		if (IS_SERVER) {
+			const areEntriesAlreadyInjected = all(
+				entry => includes(entry, getEntries(store)),
+				createEntries(injectables, props)
+			);
+
+			if (!areEntriesAlreadyInjected) {
+				warn(
+					`When rendering on the server, inject all ${type} before calling`,
+					"'ReactDOMServer.renderToString()'. You should do this inside an",
+					"'async getInitialProps()' function, i.e. where you fetch data and",
+					'do other side effects. If you need to do server-side injections',
+					'during rendering, open an issue.'
 				);
+			}
+		}
 
-				if (!areEntriesAlreadyInjected) {
-					warn(
-						`When rendering on the server, inject all ${type} before calling`,
-						"'ReactDOMServer.renderToString()'. You should do this inside an",
-						"'async getInitialProps()' function, i.e. where you fetch data and",
-						'do other side effects. If you need to do server-side injections',
-						'during rendering, open an issue.'
-					);
-				}
+		// NOTE: This doesn't run on the server, but won't trigger `useLayoutEffect` warnings either.
+		useUniversalLayoutEffect(() => {
+			if (isGlobal && feature !== DEFAULT_FEATURE) {
+				warn(
+					`You are using a feature (${feature}) with global ${type}.`,
+					'This will have no effect.'
+				);
 			}
 
-			// NOTE: This doesn't run on the server, but won't trigger `useLayoutEffect` warnings either.
-			useUniversalLayoutEffect(() => {
-				if (isGlobal && feature !== DEFAULT_FEATURE) {
-					warn(
-						`You are using a feature (${feature}) with global ${type}.`,
-						'This will have no effect.'
-					);
+			if (!namespace && !isGlobal) {
+				warn(
+					`You're injecting ${type} with no namespace!`,
+					'They will be injected globally. If this is intended, consider passing',
+					`'isGlobal: true' to the injector, e.g. '${hookName}(${type}, { isGlobal: true })'.`
+				);
+			}
+
+			if (isNotNil(options.global)) {
+				warn(`'global: ${options.global}' is deprecated. Use 'isGlobal: ${options.global}'.`);
+			}
+
+			if (isNotNil(options.persist)) {
+				warn(
+					`'persist: ${options.persist}' is deprecated. Use 'isPersistent: ${options.persist}'.`
+				);
+			}
+
+			inject(store)(injectables, props);
+			setIsInitialized(true);
+
+			return () => {
+				if (!isPersistent) {
+					eject(store)(injectables, props);
 				}
+			};
+		}, []);
 
-				if (!namespace && !isGlobal) {
-					warn(
-						`You're injecting ${type} with no namespace!`,
-						'They will be injected globally. If this is intended, consider passing',
-						`'isGlobal: true' to the injector, e.g. '${hookName}(${type}, { isGlobal: true })'.`
-					);
-				}
-
-				if (isNotNil(options.global)) {
-					warn(`'global: ${options.global}' is deprecated. Use 'isGlobal: ${options.global}'.`);
-				}
-
-				if (isNotNil(options.persist)) {
-					warn(
-						`'persist: ${options.persist}' is deprecated. Use 'isPersistent: ${options.persist}'.`
-					);
-				}
-
-				inject(store)(injectables, props);
-				setIsInitialized(true);
-
-				return () => {
-					if (!isPersistent) {
-						eject(store)(injectables, props);
-					}
-				};
-			}, []);
-
-			return isInitialized;
-		},
-	}[hookName];
+		return isInitialized;
+	};
 
 	return useInjectables;
 };

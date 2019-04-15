a476efd34d0f04bd8ef1c85a29422b930e1446cd
Václav Jančařík <vaclav.janc@gmail.com>
2019-04-15 16:29:50 +0200

    Add basic makeDecorator and makeHook tests

--- packages/injectors-react/src/makeDecorator.test.js
+++ packages/injectors-react/src/makeDecorator.test.js
@@ -1,5 +1,39 @@
+import React from 'react';
+import { makeConfig } from '@redux-tools/injectors';
+import { noop } from 'ramda-extension';
+import { mount } from 'enzyme';
+
+import makeDecorator from './makeDecorator';
+import Provider from './Provider';
+
+const config = makeConfig('things');
+const withThings = makeDecorator(config);
+
+jest.mock('./constants', () => ({ IS_SERVER: false }));
+
 describe('makeDecorator', () => {
-	it('works', () => {
-		// TODO: Write tests.
+	const store = {
+		injectThings: jest.fn(),
+		ejectThings: jest.fn(),
+		subscribe: jest.fn(),
+		getState: jest.fn(),
+		dispatch: jest.fn(),
+	};
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('calls proper store methods when mounted', () => {
+		const Root = withThings({ foo: noop })(noop);
+
+		mount(
+			<Provider store={store} namespace="yolo">
+				<Root />
+			</Provider>
+		);
+
+		expect(store.injectThings).toHaveBeenCalledTimes(1);
+		expect(store.injectThings.mock.calls[0][0]).toEqual({ foo: noop }, { namespace: 'yolo' });
 	});
 });

d8f5b6a5fdad156d4785f69464b8860697062012
Václav Jančařík <vaclav.janc@gmail.com>
2019-08-03 08:59:09 +0200

    remove unused functions

--- packages/namespaces/src/index.js
+++ packages/namespaces/src/index.js
@@ -1,6 +1,4 @@
 export { attachNamespace, defaultNamespace } from './attachNamespace';
-export { default as attachFeature } from './attachFeature';
 export { default as getNamespaceByAction } from './getNamespaceByAction';
-export { default as getFeatureByAction } from './getFeatureByAction';
 export { default as isActionFromNamespace } from './isActionFromNamespace';
 export * from './constants';

d209bddb1bb9f43e0312405f860867de0efa9217
Veronika Wernerová <vwerne@gmail.com>
2020-03-16 15:20:02 +0100

    #110 Remove reexport

--- packages/reducers/src/index.js
+++ packages/reducers/src/index.js
@@ -1,4 +1,3 @@
 export { default as makeEnhancer, storeInterface } from './makeEnhancer';
 export { default as composeReducers } from './composeReducers';
 export { default as makeReducer } from './makeReducer';
-export { getStateByAction, getStateByNamespace } from '@redux-tools/namespaces';
